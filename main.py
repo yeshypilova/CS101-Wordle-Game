d72ce7df3c164511df2c652621fc821bdfff0c41
dayttsu <humanbeanel@gmail.com>
2025-12-04 07:06:01 +0200

    Перевірка кількості літер в слові користувача

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ attempts = 6
 now_attempt = 0
 
 print("ВІТАЄМО У WORDLE")
-print(f"У вас є {attempts} спроб, щоб вгадати слово з 5 літер.\n")
+print(f"У вас є 6 спроб, щоб вгадати слово з 5 літер.\n")
 
 while now_attempt < attempts:
     now_attempt += 1
     guess = input(f"Спроба {now_attempt}/{attempts}:\nВведіть слово: ").lower()
+
+    if len(guess) != 5:
+        print("Слово повинно містити 5 літер\n")
+        now_attempt -= 1
+        continue
 
     if guess == hidden_word:
         print(f"Ви вгадали слово {hidden_word} за {now_attempt} спроб(и)!")
