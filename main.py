f45769019e81aa11492563cc7529dec53293f89c
dayttsu <humanbeanel@gmail.com>
2025-12-04 07:07:36 +0200

    Встановлення колорами

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 
 words = ("кухня", "чобіт", "човен", "любов", "щастя", "мотор", "кожух", "геній",
-         "масло", "мийка", "риска", "кішка", "сирок", "парта", "дошка", "груша",
-         "курка", "заєць", "війна", "ручка", "пушка", "пишка", "мишка", "мошка",
-         "байка", "варка")
+         "масло", "мийка", "риска", "кішка", "сирок", "парта", "дошка", "груша","курка", "заєць", "війна",
+         "ручка", "пушка", "пишка", "мишка", "мошка","байка", "варка")
 
 hidden_word = random.choice(words)
-
 attempts = 6
 now_attempt = 0
 
@@ -22,10 +23,19 @@ while now_attempt < attempts:
         now_attempt -= 1
         continue
 
+    result = []
+    for i in range(5):
+        if guess[i] == hidden_word[i]:
+            result.append(Fore.GREEN + guess[i] + Style.RESET_ALL)
+        elif guess[i] in hidden_word:
+            result.append(Fore.YELLOW + guess[i] + Style.RESET_ALL)
+        else:
+            result.append(Fore.WHITE + guess[i] + Style.RESET_ALL)
+
+    print( " ".join(result), "\n")
+
     if guess == hidden_word:
-        print(f"Ви вгадали слово {hidden_word} за {now_attempt} спроб(и)!")
+        print(f"Ви вгадали наше слово {Fore.GREEN + hidden_word.upper()} за {now_attempt} спроб(и)!")
         break
-    else:
-        print("Неправильно.\n")
 else:
-    print(f"На жаль, ви не вгадали слово. Загадане слово: {hidden_word}")
+    print(f"На жаль, ви не вгадали слово. Ми загадали слово: {hidden_word.upper()}")
